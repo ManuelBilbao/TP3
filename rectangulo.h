ca30a6cc17c8213f999d57ad9959254103f64b58
Giaco <43766886+Giacocipo@users.noreply.github.com>
2018-10-24 15:38:51 -0300

    Update rectangulo.h

--- rectangulo.h
+++ rectangulo.h
@@ -10,8 +10,16 @@ class Rectangulo : public Figuras {
     double altura;
     double base;
   public:
+  
+    //constructor
     Rectangulo(double altura, double base);
+   
+    //pre: -
+    //pos: devuelve la superficie del rectangulo
     double calcular_superficie();
+  
+    //pre: -
+    //pos: devuelve el perimetro del rectangulo
     double calcular_perimetro();
 
 };
