256222f57d34a88ad9324c7362c2e1147a26dabf
kinirochan <43797395+kinirochan@users.noreply.github.com>
2018-10-23 22:43:30 -0300

    Add files via upload

--- lista.cpp
+++ lista.cpp
@@ -1,69 +1,76 @@
 #include "lista.h"
 
+int const POSICION_INICIO_LISTA = 1;
+int const LONGITUD_LISTA_VACIA = 0;
+
 //Constructor
-Lista::Lista() {
+Lista::Lista () {
 	this->primero = 0;
 	this->longitud = 0;
 }
 
 //Destructor
-Lista::~Lista() {
-	while (longitud > 0) {
-		borrar(1);
+Lista::~Lista () {
+	while (longitud > LONGITUD_LISTA_VACIA) {
+		borrar_elemento (1);
 	}
 }
 
 unsigned Lista::obtener_longitud() {
-	return this->longitud;
+	return this -> longitud;
 }
 
-bool Lista::lista_vacia() {
-	return (longitud == 0);
+bool Lista::lista_vacia () {
+	return (longitud == LONGITUD_LISTA_VACIA);
 }
 
-void Lista::insertar(Tipo* e, unsigned pos) {
-	Nodo* nuevo = new Nodo(e);
+void Lista::insertar (Tipo* elemento, unsigned posicion) {
 
-	if (pos == 1) {
-		nuevo->asignar_siguiente(primero);
-		this->primero = nuevo;
+	Nodo* nuevo = new Nodo (elemento);
+
+	if (posicion == POSICION_INICIO_LISTA) {
+		nuevo -> asignar_siguiente (primero);
+		this -> primero = nuevo;
 	} else {
-		Nodo* anterior = obtener_nodo(pos - 1);
-		nuevo->asignar_siguiente(anterior->obtener_siguiente());
-		anterior->asignar_siguiente(nuevo);
+		Nodo* anterior = obtener_nodo(posicion - 1);
+		nuevo -> asignar_siguiente (anterior -> obtener_siguiente ());
+		anterior -> asignar_siguiente (nuevo);
 	}
 
-	this->longitud++;
+	this -> longitud++;
 }
 
 //Método privado
-Nodo* Lista::obtener_nodo(unsigned pos) {
-	Nodo* aux = primero;
+Nodo* Lista::obtener_nodo (unsigned posicion) {
 
-	for (unsigned i = 1; i < pos; i++) {
-		aux = aux->obtener_siguiente();
+	Nodo* auxiliar = primero;
+
+	for (unsigned i = 1; i < posicion; i++) {
+		auxiliar = auxiliar -> obtener_siguiente ();
 	}
 
-	return aux;
+	return auxiliar;
 }
 
-Tipo* Lista::consultar(unsigned pos){
-	Nodo* aux = obtener_nodo(pos);
-	return (aux->obtener_dato());
+Tipo* Lista::consultar (unsigned posicion) {
+
+	Nodo* auxiliar = obtener_nodo (posicion);
+	return (auxiliar -> obtener_dato ());
 }
 
-void Lista::borrar(unsigned pos){
+void Lista::borrar_elemento (unsigned posicion) {
+
 	Nodo* borrar = primero;
 
-	if (pos == 1) {
-		primero = borrar->obtener_siguiente();
+	if (posicion == POSICION_INICIO_LISTA) {
+		primero = borrar -> obtener_siguiente ();
 	} else {
-		borrar = obtener_nodo(pos);
-		Nodo* anterior = obtener_nodo(pos - 1);
-		Nodo* siguiente = borrar->obtener_siguiente();
-		anterior->asignar_siguiente(siguiente);
+		borrar = obtener_nodo (posicion);
+		Nodo* anterior = obtener_nodo (posicion - 1);
+		Nodo* siguiente = borrar -> obtener_siguiente ();
+		anterior -> asignar_siguiente (siguiente);
 	}
 
 	delete borrar;
-	this->longitud--;
+	this -> longitud--;
 }
