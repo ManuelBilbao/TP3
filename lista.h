4b763e335790f79c296bda577c8fc1ba79e16ce2
Giaco <43766886+Giacocipo@users.noreply.github.com>
2018-10-24 14:44:37 -0300

    Add files via upload

--- lista.h
+++ lista.h
@@ -5,41 +5,41 @@
 
 class Lista {
 
-	//Atributos
+	// Atributos
 	private:
 		Nodo* primero;
 		unsigned longitud;
 
-	//Métodos
+	// Métodos
 	public:
-		//Constructor
+		// Constructor
 		Lista ();
 
-		//Destructor
+		// Destructor
 		~Lista ();
 
-		//Pre:
-		//Post:
-		unsigned obtener_longitud ();
-
-		//Pre:
-		//Post:
+		// Pre: -
+		// Post: Devuelve verdadero si la lista esta vacía
 		bool lista_vacia ();
 
-		//Pre: 0 < pos  <= obtener_longitud()+1
+		//Pre: 0 < posicion  <= obtener_longitud()+1
 		//Post: coloca el dato en la posición, la primer posición es la 1
 		void insertar (Tipo* elemento, unsigned posicion);
 
-		//Pre: 0 < pos <= obtener_longitud()
-		//Post:  quita el dato
-		void borrar_elemento (unsigned posicion);
-
-		//Pre: 0 < pos <= obtener_longitud()
+		//Pre: 0 < posicion <= obtener_longitud()
 		//Post: devuelve el dato
 		Tipo* consultar (unsigned posicion);
 
-		//Pre:
-		//Post:
+		//Pre: 0 < posicion <= obtener_longitud()
+		//Post:  quita el dato
+		void borrar_elemento (unsigned posicion);
+
+		// Pre: -
+		// Post: Devuelve la cantidad de nodos presentes en la lista
+		unsigned obtener_longitud ();
+
+		//Pre: 0 < posicion <= obtener_longitud()
+		//Post: devuelve un puntero al nodo pedido
 		Nodo* obtener_nodo (unsigned posicion);
 };
 
